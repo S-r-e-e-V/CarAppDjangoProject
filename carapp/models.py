b0d72a75146a8688e6f2a1c52c09805844d94676
Sreekanth V <25sreekanth@gmail.com>
2023-06-07 14:18:36 -0400

    DB update

--- carapp/models.py
+++ carapp/models.py
@@ -13,11 +13,10 @@ class CarType(models.Model):
 class Vehicle(models.Model):
     car_type = models.ForeignKey(CarType, related_name='vehicles', on_delete=models.CASCADE)
     car_name = models.CharField(max_length=200)
-    car_price = models.DecimalField(max_digits=10, decimal_places=6)
+    car_price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.PositiveIntegerField(default=10)
     instock = models.BooleanField(default=True)
-
-    # description = models.TextField(max_length=500,blank=True)
+    description = models.TextField(max_length=500, blank=True)
 
     def __str__(self):
         return self.car_name
@@ -31,7 +30,8 @@ class Buyer(User):
         ('L', 'Lakeshore'),
         ('LE', 'Leamington'),
         ('CH', 'Chatham'),
-        ('T', 'Toronto')]
+        ('T', 'Toronto'),
+        ('Wl', "Waterloo")]
 
     shipping_address = models.CharField(max_length=300, null=True, blank=True)
     area = models.CharField(max_length=2, choices=AREA_CHOICES, default='CH')
@@ -44,11 +44,11 @@ class Buyer(User):
 
 class OrderVehicle(models.Model):
     STATUS_VALUES = [
-        (0, 0),
-        (1, 1),
-        (2, 2),
-        (3, 3),
-        (4, 4), ]
+        (0, "cancelled"),
+        (1, "placed"),
+        (2, "shipped"),
+        (3, "delivered"),
+    ]
     buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
     number_of_Vehicles = models.IntegerField(default=0)
