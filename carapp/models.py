aa810fb459ebac5b5c4f492e46594791b44bdf67
Sreekanth V <25sreekanth@gmail.com>
2023-07-06 12:35:18 -0400

    feat: lab7 models

--- carapp/models.py
+++ carapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
 class CarType(models.Model):
     name = models.CharField(max_length=150)
@@ -9,14 +8,35 @@ class CarType(models.Model):
     def __str__(self):
         return self.name
 
-
 class Vehicle(models.Model):
+    CRUISE_CONTROL = 'Cruise Control'
+    AUDIO_INTERFACE = 'Audio Interface'
+    AIRBAGS = 'Airbags'
+    AIR_CONDITIONING = 'Air Conditioning'
+    SEAT_HEATING = 'Seat Heating'
+    PARK_ASSIST = 'ParkAssist'
+    POWER_STEERING = 'Power Steering'
+    REVERSING_CAMERA = 'Reversing Camera'
+    AUTO_START_STOP = 'Auto Start/Stop'
+
+    CAR_FEATURES = [
+        (CRUISE_CONTROL, 'Cruise Control'),
+        (AUDIO_INTERFACE, 'Audio Interface'),
+        (AIRBAGS, 'Airbags'),
+        (AIR_CONDITIONING, 'Air Conditioning'),
+        (SEAT_HEATING, 'Seat Heating'),
+        (PARK_ASSIST, 'ParkAssist'),
+        (POWER_STEERING, 'Power Steering'),
+        (REVERSING_CAMERA, 'Reversing Camera'),
+        (AUTO_START_STOP, 'Auto Start/Stop'),
+    ]
     car_type = models.ForeignKey(CarType, related_name='vehicles', on_delete=models.CASCADE)
     car_name = models.CharField(max_length=200)
     car_price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.PositiveIntegerField(default=10)
     instock = models.BooleanField(default=True)
     description = models.TextField(max_length=500, blank=True)
+    car_features = models.CharField(max_length=100, choices=CAR_FEATURES, blank=True, null=True)
 
     def __str__(self):
         return self.car_name
